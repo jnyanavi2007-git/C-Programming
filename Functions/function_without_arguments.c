73a3d147869574b8324b2bde2fc5b875be41422f
Jnyanavi Durgekar <jnyanavidurgekar2007@gmail.com>
2026-07-08 23:30:18 +0530

    Create function_without_arguments.c

---
+++ Functions/function_without_arguments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Function declaration
+void display();
+
+// Main function
+int main() {
+    display();   // Function call
+    return 0;
+}
+
+// Function definition
+void display() {
+    printf("Welcome to C Programming!\n");
+}
+
+Output:
+Welcome to C Programming!
