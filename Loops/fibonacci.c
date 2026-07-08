e5b5386cc732e97085d5ea7f59c8ea66e09ba910
Jnyanavi Durgekar <jnyanavidurgekar2007@gmail.com>
2026-07-08 23:25:25 +0530

    Create fibonacci.c

---
+++ Loops/fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 0, b = 1, c;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
+
+Output:
+Enter number of terms: 10
+Fibonacci Series:
+0 1 1 2 3 5 8 13 21 34
