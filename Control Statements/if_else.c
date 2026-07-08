1f560e0931cbdb4e77b057a25ecb164176b1915c
Jnyanavi Durgekar <jnyanavidurgekar2007@gmail.com>
2026-07-08 23:05:14 +0530

    Create if_else.c

---
+++ Control Statements/if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is Even.\n", num);
+    else
+        printf("%d is Odd.\n", num);
+
+    return 0;
+}
+
+Output:
+Enter a number: 17
+17 is Odd.
